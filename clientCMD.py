03d211059d2689a0779ef70325ee518bbb2b13b1
Prerak Lodha <57831679+prerakl123@users.noreply.github.com>
2021-02-21 12:10:25 +0530

    Create clientCMD.py

---
+++ clientCMD.py
@@ -0,0 +1,39 @@
+from socket import *
+from threading import Thread
+
+
+def receive():
+    while True:
+        msg = client_socket.recv(BUFSIZ).decode("utf8")
+        if msg == "{quit}":
+            client_socket.close()
+            break
+        if not msg:
+            break
+        print(msg)
+
+
+def send():
+    while True:
+        msg = input()
+        client_socket.send(bytes(msg, "utf8"))
+        if msg == "{quit}":
+            break
+
+
+PORT = 33000
+HOST = '127.0.0.1'
+
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+send_thread = Thread(target=send)
+receive_thread.start()
+send_thread.start()
+receive_thread.join()
+send_thread.join()
